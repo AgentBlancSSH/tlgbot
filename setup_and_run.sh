591a849bd2246906cd0f32e0d97cdab0a26102a8
Agent Blanc <177425734+AgentBlancSSH@users.noreply.github.com>
2024-08-10 05:49:09 +0200

    Update setup_and_run.sh

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
-# Mettre à jour le système
-echo "Mise à jour du système..."
-sudo apt-get update -y
-sudo apt-get upgrade -y
+# Nom du script Python
+PYTHON_SCRIPT="bot_scipt.py"
 
-# Installer Python et pip si ce n'est pas déjà fait
-echo "Installation de Python et pip..."
-sudo apt-get install -y python3 python3-pip
+# Fonction pour vérifier si un paquet est installé
+function check_install {
+    if ! command -v $1 &> /dev/null
+    then
+        echo "$1 n'est pas installé. Installation en cours..."
+        sudo apt-get install -y $1
+    else
+        echo "$1 est déjà installé."
+    fi
+}
 
-# Installer les dépendances Python
-echo "Installation des dépendances Python..."
+# Vérification et installation de Python
+check_install python3
+
+# Vérification et installation de pip
+check_install pip3
+
+# Mise à jour de pip
+echo "Mise à jour de pip..."
 pip3 install --upgrade pip
-pip3 install python-telegram-bot
 
-# Assurez-vous que le script Python est exécutable
-echo "Rendre le script Python exécutable..."
-chmod +x bot_script.py
+# Liste des packages Python nécessaires
+REQUIRED_PIP_PACKAGES=(
+    "python-telegram-bot"
+    "matplotlib"
+)
 
-# Lancer le script Python
-echo "Lancement du script Python..."
-python3 bot_script.py
+# Installation des packages Python nécessaires
+for PACKAGE in "${REQUIRED_PIP_PACKAGES[@]}"; do
+    if ! pip3 show $PACKAGE &> /dev/null
+    then
+        echo "Le package Python $PACKAGE n'est pas installé. Installation en cours..."
+        pip3 install $PACKAGE
+    else
+        echo "Le package Python $PACKAGE est déjà installé."
+    fi
+done
 
-# Indiquer que tout s'est bien passé
-echo "Installation terminée et script lancé avec succès."
+# Mise à jour des packages Python installés
+echo "Mise à jour des packages Python..."
+pip3 list --outdated | grep -o '^\S*' | xargs -n1 pip3 install -U
+
+# Lancement du script Python
+echo "Lancement du script Python $PYTHON_SCRIPT..."
+python3 $PYTHON_SCRIPT
